90f89fe0029bc89870cd10f09bc42430a31ecc9c
buihoangnam1988 <buihoangnam1988@gmail.com>
2024-06-11 19:36:24 +0900

    Initial commit

---
+++ src/test/java/dev/hnbui/redis_session/RedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hnbui.redis_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
